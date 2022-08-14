68608945dd03992cabdf9f1e4d25905578b44f93
mohamedhamdy2017 <34789872+mohamedhamdy2017@users.noreply.github.com>
2022-08-14 21:33:10 +0200

    update colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,6 +2,8 @@ export const colors = {
   white: '#ffffff',
   red: 'red',
   blue: 'blue',
-  gray:'gray',
-  lightGray: '#EEE'
+  gray: 'gray',
+  lightGray: '#EEE',
+  black: 'black',
+  yellow: '#ebc634',
 };
